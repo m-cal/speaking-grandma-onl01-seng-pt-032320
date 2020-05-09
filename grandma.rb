d877e9c6c429074befa8f571b1f4e01458667dbd
Michael Calhoun <iengineersoftware@gmail.com>
2020-05-09 01:31:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(words)
   if words != words.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif words = "I LOVE YOU GRANDMA!"
+  elsif words == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
