1f3519ab4f6de5d0c914451576385fa16e4015f0
Michael Calhoun <iengineersoftware@gmail.com>
2020-05-09 01:25:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(words)
-  if words.upcase != words
-    puts 'HUH?! SPEAK UP, SONNY!'
-  elsif words == "I LOVE YOU GRANDMA!"
+  if words = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else
-    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
